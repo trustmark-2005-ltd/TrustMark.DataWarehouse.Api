237287da04ab83296cb2bec0e0aac6126252ef68
Oliver Booth <obooth@preppygreen.com>
2020-04-06 09:49:16 +0100

    Add UMR to AddMeasureResponse

--- src/dotnetcore/SampleApp/Models/AddMeasureResponse.cs
+++ src/dotnetcore/SampleApp/Models/AddMeasureResponse.cs
@@ -1,10 +1,18 @@
-﻿namespace SampleApp.Models
+﻿using Newtonsoft.Json;
+
+namespace SampleApp.Models
 {
     public class AddMeasureResponse
     {
+        [JsonProperty("status")]
         public string Status { get; set; }
+        [JsonProperty("message")]
         public string Message { get; set; }
+        [JsonProperty("measureId")]
         public string MeasureId { get; set; }
+        [JsonProperty("umr")]
+        public string UMR { get; set; }
+        [JsonProperty("measure")]
         public Measure Measure { get; set; }
     }
 }
